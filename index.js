7b88fdb749d427ad19598c3bdb6d89827498fe4d
Kishor Krishnan <Blizzcane@gmail.com>
2022-04-21 13:20:17 -0500

    added hover effect

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import * as THREE from 'https://unpkg.com/three@0.139.2/build/three.module.js'
 import { OrbitControls } from 'https://unpkg.com/three@0.139.2/examples/jsm/controls/OrbitControls.js'
 
 //create a scene, camera, and renderer
+const raycaster = new THREE.Raycaster();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, innerWidth / innerHeight, 0.1, 1000);
 const renderer = new THREE.WebGLRenderer();
@@ -13,11 +14,11 @@ document.body.appendChild(renderer.domElement); //inject canvas element
 //need a geometry: data relating to all of the object's vertices that connects into a shape
 //need a material that fills in the wireframe of the geometry
 
-new OrbitControls(camera,renderer.domElement);
+new OrbitControls(camera, renderer.domElement);
 camera.position.z = 5 //move the box backwards so we can see the scene
 
 const planeGeometry = new THREE.PlaneGeometry(5, 5, 10, 10);
-const planeMaterial = new THREE.MeshPhongMaterial({ color: 0xFF0000, side: THREE.DoubleSide, flatShading: THREE.FlatShading });
+const planeMaterial = new THREE.MeshPhongMaterial({ side: THREE.DoubleSide, flatShading: THREE.FlatShading, vertexColors: true });
 const planeMesh = new THREE.Mesh(planeGeometry, planeMaterial);
 
 scene.add(planeMesh); //adds items to scene
@@ -30,16 +31,46 @@ for (let i = 0; i < array.length; i += 3) {
     array[i + 2] = z + Math.random();
 }
 
+const colors = [];
+for (let i = 0; i < planeMesh.geometry.attributes.position.count; i++) {
+    colors.push(1, 0, 0);
+}
+
+planeMesh.geometry.setAttribute('color', new THREE.BufferAttribute(new Float32Array(colors), 3));
+
 const light = new THREE.DirectionalLight(0xffffff, 1);
 light.position.set(0, 0, 1);
 scene.add(light);
 
+const backLight = new THREE.DirectionalLight(0xffffff, 1);
+backLight.position.set(0, 0, -1);
+scene.add(backLight);
+
+const mouse = {
+    x: undefined,
+    y: undefined
+}
+
 function animate() {
     requestAnimationFrame(animate);
     renderer.render(scene, camera);
+    raycaster.setFromCamera(mouse, camera);
+    const intersects = raycaster.intersectObject(planeMesh);
+    if (intersects.length > 0) {
+        const { color } = intersects[0].object.geometry.attributes;
+        color.setX(intersects[0].face.a, 0);
+        color.setX(intersects[0].face.b, 0);
+        color.setX(intersects[0].face.c, 0);
+        color.needsUpdate = true;
+
+    }
 }
 
 //renders scene
 
 animate();
 
+addEventListener('mousemove', (event) => {
+    mouse.x = (event.clientX / innerWidth) * 2 - 1;
+    mouse.y = -(event.clientY / innerHeight) * 2 + 1;
+})
